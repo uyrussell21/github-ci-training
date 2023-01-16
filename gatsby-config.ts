0a2777118b26f42c237a5a35bfbfa19edfb4dd18
Russell Uy <uyrussell@gmail.com>
2023-01-16 15:18:05 +0800

    fix gatsby config for pathprefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,8 +4,8 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `Gatsby GitHub Site`,
     siteUrl: `https://uyrussell21.github.io`,
-    pathPrefix: "/github-ci-exercise",
   },
+  pathPrefix: "/github-ci-exercise",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
